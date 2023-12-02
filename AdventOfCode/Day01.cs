1e6db0182fb1a0fcf358b43125fe0bcddf4e6ea7
MinixBF <benj.ferre@hotmail.fr>
2023-12-02 22:45:21 +0100

    Day 1 part 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,25 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly List<string> _input;
+    private readonly int _result;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        // Read input file
+        _input = File.ReadAllLines(InputFilePath).ToList();
+
+        // Get only number 
+        _input = _input.Select(x => Regex.Replace(x, @"[^0-9]", "")).ToList();
+        // Agregate first and last number. Sum all
+        _result = _input.Select(x => x[0] + x[^1].ToString()).ToList().Sum(x => int.Parse(x));
+
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is {_result}");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
